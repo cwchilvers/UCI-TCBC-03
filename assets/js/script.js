cec39eab4078920dbb0cbad94a57a18488a5ec5a
cwchilvers <cwchilvers@gmail.com>
2023-04-15 16:49:43 -0700

    Validation for first prompt (password length)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,22 @@
 // Assignment code here
 function generatePassword() {
-  var i = 1;
-  var password = "";
-  let lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  let uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  let numbers = ["0","1","2","3","4","5","6","7","8","9"];
-  let special = ["~","\`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|",":",";","<",">","?",".","\\", "\""];
-  var passwordArray = [];
+  let password = "";
+  let passwordArray = [];
+  let i = 1;
+  let length = 0;
+  let validInput = false;
+  const lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  const uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  const numbers = ["0","1","2","3","4","5","6","7","8","9"];
+  const special = ["~","\`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|",":",";","<",">","?",".","\\", "\""];
 
   // Ask user for password length
-  length = prompt("Enter Password Length (8-128 Characters):");
+  while (validInput === false) {
+    length = prompt("Enter Password Length (8-128 Characters):");
+    if (length >= 8 && length <= 128) {
+      validInput = true;
+    }
+  }
 
   // Ask user for lowercase characters
   if (confirm("Use lowercase characters?") == true) {
@@ -28,6 +35,7 @@ function generatePassword() {
     passwordArray = passwordArray.concat(special);
   } 
 
+  // Create password
   while (i <= length) {
     password = password.concat(passwordArray[Math.floor(Math.random()*passwordArray.length)]);
     i++;

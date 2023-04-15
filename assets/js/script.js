258ef3ae2ef71be5373e9c9ad7ac712f672ee042
cwchilvers <cwchilvers@gmail.com>
2023-04-15 16:29:06 -0700

    Deleted some unecessary variables. Asks user several different prompts regarding characters to use. If user confirms, characters are added to a master array for password generation.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,32 @@
 // Assignment code here
 function generatePassword() {
-  var userInput;
-  var letter;
-  var number;
-  var symbol;
   var i = 1;
   var password = "";
-  let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  let lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  let uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   let numbers = ["0","1","2","3","4","5","6","7","8","9"];
-  let symbols = ["~","\`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|",":",";","<",">","?",".","\\", "\""];
+  let special = ["~","\`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|",":",";","<",">","?",".","\\", "\""];
   var passwordArray = [];
 
+  // Ask user for password length
   length = prompt("Enter Password Length (8-128 Characters):");
 
+  // Ask user for lowercase characters
+  if (confirm("Use lowercase characters?") == true) {
+    passwordArray = passwordArray.concat(lowercase);
+  } 
+  // Ask user for uppercase characters
+  if (confirm("Use uppercase characters?") == true) {
+    passwordArray = passwordArray.concat(uppercase);
+  } 
+  // Ask user for numbers
+  if (confirm("Use numbers?") == true) {
+    passwordArray = passwordArray.concat(numbers);
+  } 
+  // Ask user for special characters
+  if (confirm("Use special characters?") == true) {
+    passwordArray = passwordArray.concat(special);
+  } 
 }
 
 // Get references to the #generate element

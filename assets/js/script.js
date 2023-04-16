5139d8e28df8a9e3caa731250affeba71c1b15b9
cwchilvers <cwchilvers@gmail.com>
2023-04-15 17:16:44 -0700

    Changed another while loop to a for loop and eliminated an unused variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,16 @@
 function generatePassword() {
   let password = "";
   let passwordArray = [];
-  let validInput = false;
   const lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   const uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   const numbers = ["0","1","2","3","4","5","6","7","8","9"];
   const special = ["~","\`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|",":",";","<",">","?",".","\\", "\""];
 
   // Ask user for password length
-  while (validInput === false) {
+  for (i = 0; i != 1; i) {
     length = prompt("Enter Password Length (8-128 Characters):");
     if (length >= 8 && length <= 128) {
-      validInput = true;
+      i++;
     }
   }
 

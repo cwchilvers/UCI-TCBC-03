2227ab7e1a69188b8fe001be65cfbc43b7375def
cwchilvers <cwchilvers@gmail.com>
2023-04-15 17:14:13 -0700

    Removed uneccessary variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@
 function generatePassword() {
   let password = "";
   let passwordArray = [];
-  let i = 1;
   let validInput = false;
   const lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   const uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];

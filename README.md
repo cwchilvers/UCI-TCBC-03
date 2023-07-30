3be566951763e2915d4aa72716ea4d17e2d168da
Chandler Chilvers <cwchilvers@gmail.com>
2023-07-30 16:41:44 -0700

    Restructured code. Updated README.

--- README.md
+++ README.md
@@ -1,9 +1,39 @@
-# Challenge 3: Random Password Generator
+<h1 align="center">
+  Random Password Generator
+</h1>
+
+<p align="center">
+    <img src="https://img.shields.io/badge/HTML5-E34F26.svg?style=for-the-badge&logo=HTML5&logoColor=white" alt="HTML5">
+    <img src="https://img.shields.io/badge/CSS3-1572B6.svg?style=for-the-badge&logo=CSS3&logoColor=white" alt="CSS3">
+    <img src="https://img.shields.io/badge/JavaScript-F7DF1E.svg?style=for-the-badge&logo=JavaScript&logoColor=black" alt="JavaScript">
+    <img src="https://img.shields.io/badge/License-MIT-blue.svg?style=for-the-badge" alt="MIT License">
+</p>
+
+<h4 align="center">A simple simple random password generator.</h4>
+
+## Table of Contents
+1. [Description](#description)
+2. [Usage](#usage)
+3. [Technologies Used](#technologies-used)
+4. [License](#license)
+5. [Contact Information](#contact-information)
+
 ## Description
-This application helps a user generate a random password. The user can specify the length of the password and the types of characters used.  
+This is a simple random password generator that allows the user to select the length of the password and which character types to include. The user can choose to include lowercase letters, uppercase letters, numbers, and/or special characters. The password is generated randomly and displayed on the page.
 
-## Screenshot
-![Screenshot 2023-04-15 231129](https://user-images.githubusercontent.com/59628271/232275236-3f22115c-fbd4-4a3d-9aff-93bd5287d332.png)
+## Usage
+To use this web application, either access the web app [here](https://example.com) or clone the repository locally and open the `index.html` file in your browser. To generate a password, click the "Generate Password" button. You will be prompted to enter a password length between 8 and 128 characters. After entering a valid password length, you will be prompted to select which character types to include in the password. You must select at least one character type. After selecting the character types, a password will be generated and displayed on the page. If you would like to generate another password, click the "Generate Password" button again.
 
-## Deployed Web App
-[Random Password Generator](https://apps.cwchilvers.io/apps/uci-bootcamp/UCI-CBC-03-PasswordGenerator/index.html)
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+
+## License
+This project is licensed under the MIT License. See the [MIT License](https://opensource.org/licenses/mit/) page for details.
+
+## Contact Information
+<p align="center">
+    <a href="mailto:cwchilvers@gmail.com"><img src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white" alt="Gmail"></a>
+    <a href="https://github.com/cwchilvers"><img src="https://img.shields.io/badge/GitHub-181717.svg?style=for-the-badge&logo=GitHub&logoColor=white" alt="GitHub"></a>
+</p>
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,43 @@
-// Assignment code here
-function generatePassword() {
+const generatePassword = () => {
+  const charSets = {
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers: "0123456789",
+    special: "~`!@#$%^&*()_-+={[]}|:;<>,.?\\\"",
+  };
+
   let password = "";
-  let passwordArray = [];
-  const lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  const uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  const numbers = ["0","1","2","3","4","5","6","7","8","9"];
-  const special = ["~","\`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|",":",";","<",">","?",".","\\", "\""];
+  let charSelection = "";
 
   // Ask user for password length
-  for (i = 0; i != 1; i) {
+  let length;
+  do {
     length = prompt("Enter Password Length (8-128 Characters):");
-    if (length >= 8 && length <= 128) {
-      i++;
-    }
-  }
+  } while (length < 8 || length > 128);
 
-  while (passwordArray.length === 0) {
-    // Ask user for lowercase characters
-    if (confirm("Use lowercase characters?") == true) {
-      passwordArray = passwordArray.concat(lowercase);
-    } 
-    // Ask user for uppercase characters
-    if (confirm("Use uppercase characters?") == true) {
-      passwordArray = passwordArray.concat(uppercase);
-    } 
-    // Ask user for numbers
-    if (confirm("Use numbers?") == true) {
-      passwordArray = passwordArray.concat(numbers);
-    } 
-    // Ask user for special characters
-    if (confirm("Use special characters?") == true) {
-      passwordArray = passwordArray.concat(special);
-    } 
-    // If user selects nothing
-    if (passwordArray.length === 0) {
-      alert("Please select at least one character type.");
-    }
+  // Ask user for character types
+  if (confirm("Use lowercase characters?")) charSelection += charSets.lowercase;
+  if (confirm("Use uppercase characters?")) charSelection += charSets.uppercase;
+  if (confirm("Use numbers?")) charSelection += charSets.numbers;
+  if (confirm("Use special characters?")) charSelection += charSets.special;
+
+  // If user selects nothing
+  if (charSelection.length === 0) {
+    alert("Please select at least one character type.");
+    return "";
   }
 
   // Create password
-  for (i = 1; i <= length; i++) {
-    password = password.concat(passwordArray[Math.floor(Math.random()*passwordArray.length)]);
+  for (let i = 1; i <= length; i++) {
+    password += charSelection[Math.floor(Math.random() * charSelection.length)];
   }
-
   return password;
 }
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+const writePassword = () => {
+  document.querySelector("#password").value = generatePassword();
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+document.querySelector("#generate").addEventListener("click", writePassword);
